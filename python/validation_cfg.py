d5623f8d1c761e839f3dd86cfa623f25e6094817
Joe Crowley <mistaacrowley@gmail.com>
2022-10-07 01:51:44 +0200

    add validation_cfg template

---
+++ python/validation_cfg.py
@@ -0,0 +1,48 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process("CSCValidation",eras.Run3)
+
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'GLOBALTAG_REPLACETAG', '')
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.options = cms.untracked.PSet( SkipEvent = cms.untracked.vstring('ProductNotFound') )
+process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring('FILENAME_REPLACETAG'))
+
+process.cscValidation = cms.EDAnalyzer("CSCValidation",
+    rootFileName = cms.untracked.string('cscv_RAW.root'),
+    isSimulation = cms.untracked.bool(False),
+    writeTreeToFile = cms.untracked.bool(True),
+    useDigis = cms.untracked.bool(True),
+    detailedAnalysis = cms.untracked.bool(False),
+    useTriggerFilter = cms.untracked.bool(False),
+    useQualityFilter = cms.untracked.bool(False),
+    makeStandalonePlots = cms.untracked.bool(False),
+    makeTimeMonitorPlots = cms.untracked.bool(True),
+    rawDataTag = cms.InputTag("rawDataCollector"),
+    alctDigiTag = cms.InputTag("muonCSCDigis","MuonCSCALCTDigi"),
+    clctDigiTag = cms.InputTag("muonCSCDigis","MuonCSCCLCTDigi"),
+    corrlctDigiTag = cms.InputTag("muonCSCDigis","MuonCSCCorrelatedLCTDigi"),
+    stripDigiTag = cms.InputTag("muonCSCDigis","MuonCSCStripDigi"),
+    wireDigiTag = cms.InputTag("muonCSCDigis","MuonCSCWireDigi"),
+    compDigiTag = cms.InputTag("muonCSCDigis","MuonCSCComparatorDigi"),
+    cscRecHitTag = cms.InputTag("csc2DRecHits"),
+    cscSegTag = cms.InputTag("cscSegments"),
+    saMuonTag = cms.InputTag("standAloneMuons"),
+    l1aTag = cms.InputTag("gtDigis"),
+    hltTag = cms.InputTag("TriggerResults::HLT"),
+    makeHLTPlots = cms.untracked.bool(True),
+    simHitTag = cms.InputTag("g4SimHits", "MuonCSCHits")
+)
+
+process.p = cms.Path(process.muonCSCDigis * process.csc2DRecHits * process.cscSegments * process.cscValidation)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.schedule = cms.Schedule(process.p,process.endjob_step)
