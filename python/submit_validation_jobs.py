135537457fdd95184f6cbb296b8c5bcccf729458
joseph-crowley <mistaacrowley@gmail.com>
2022-10-06 21:25:40 -0700

    fix log location, bugs

--- python/submit_validation_jobs.py
+++ python/submit_validation_jobs.py
@@ -84,7 +84,7 @@ def submit_validation_jobs(config):
         for run in runsToUpdate:
             print(f'    Run {run}: {len(fileRunMap[run])} files, {eventRunMap[run]} events')
 
-        for run in runsToUpdate:
+        for run in runsToUpdate[:1]:
             if int(run)<MINRUN: continue
             config_tmp = config.copy()
             config_tmp.update({"runNumber":str(run)})
