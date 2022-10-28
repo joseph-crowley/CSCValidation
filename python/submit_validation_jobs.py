6286bff1303f15561829ac3a80cd4e7cec6b9ec5
Joe Crowley <crowley@ucsb.edu>
2022-10-28 15:13:00 -0700

    validate all the runs

--- python/submit_validation_jobs.py
+++ python/submit_validation_jobs.py
@@ -85,7 +85,7 @@ def submit_validation_jobs(config):
         for run in runsToUpdate:
             print(f'    Run {run}: {len(fileRunMap[run])} files, {eventRunMap[run]} events')
 
-        for run in runsToUpdate[2:3]:
+        for run in runsToUpdate:
             if int(run)<MINRUN: continue
             config_tmp = config.copy()
             config_tmp.update({"runNumber":str(run)})
